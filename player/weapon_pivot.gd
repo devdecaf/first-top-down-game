c4b230a785276892ff4deda4100e9c21d1e6cb92
wbryceangell <wbryceangell@gmail.com>
2026-05-14 07:48:43 -0500

    Add gamepad movement to weapon pivot

--- player/weapon_pivot.gd
+++ player/weapon_pivot.gd
@@ -1,8 +1,28 @@
 class_name WeaponPivot
 extends Node2D
 
+enum InputDevice {
+	MOUSE,
+	GAMEPAD,
+}
 
-func _physics_process(delta: float) -> void:
-	var direction := global_position.direction_to(get_global_mouse_position())
-	var angle := direction.angle()
-	global_rotation = rotate_toward(angle, global_rotation, delta)
+var _input_device := InputDevice.MOUSE
+
+
+func _process(delta: float) -> void:
+	var direction := (
+		global_position.direction_to(get_global_mouse_position())
+		if _input_device == InputDevice.MOUSE
+		else Input.get_vector("look_left", "look_right", "look_up", "look_down")
+	)
+
+	if direction.length() > 0.0:
+		var angle := direction.angle()
+		global_rotation = rotate_toward(angle, global_rotation, delta)
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventJoypadMotion:
+		_input_device = InputDevice.GAMEPAD
+	elif event is InputEventMouseMotion:
+		_input_device = InputDevice.MOUSE
