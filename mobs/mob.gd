81c4cdc34bf77526f673b8012324f9e3038e4f9a
wbryceangell <286201075+devdecaf@users.noreply.github.com>
2026-05-21 08:36:59 -0500

    Mob health and bullet damage
    Added health and death functionality to mob
    Added damage functionality to the bullet

--- mobs/mob.gd
+++ mobs/mob.gd
@@ -5,6 +5,8 @@ extends CharacterBody2D
 @export var max_speed := 200.0
 ## How quickly speed is added to the mob once it starts following
 @export var acceleration := 600.0
+## The amount of damage the mob can take before it dies
+@export var health := 3: set = _set_health
 
 var _player: Player = null
 
@@ -32,3 +34,16 @@ func _physics_process(delta: float) -> void:
 
 		velocity = velocity.move_toward(desired_velocity, acceleration * delta)
 		move_and_slide()
+		
+func _set_health(new_health: int) -> void:
+	health = new_health
+	if health <= 0:
+		_die()
+		
+
+func _die() -> void:
+	if is_physics_processing():
+		set_physics_process(false)
+	if not is_queued_for_deletion():
+		queue_free()
+		
--- weapons/bullets/bullet.gd
+++ weapons/bullets/bullet.gd
@@ -5,12 +5,19 @@ extends Area2D
 @export var speed := 1500.0
 ## The maximum distance the bullet will travel
 @export var max_range := 1000.0
+## The amount of damage that the bullet inflicts
+@export var damage := 1
 
 var _starting_position: Vector2
 
 
 func _ready() -> void:
 	_starting_position = position
+	body_entered.connect(func (body: Node):
+		if body is Mob:
+			(body as Mob).health -= damage
+			destroy()
+	)
 
 
 func _physics_process(delta: float) -> void:
